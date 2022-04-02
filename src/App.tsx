f5639c024e1cb057501387252653fdad1285b2f1
Abby <sk573129@gmail.com>
2022-04-03 00:14:52 +0800

    feat: 新增mui基本版型：header、menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,111 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// mui
+import { styled, useTheme } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+import CreateIcon from '@mui/icons-material/Create';
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
+
+const drawerWidth = 240;
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+	display: 'flex',
+	alignItems: 'center',
+	padding: theme.spacing(0, 1),
+	// necessary for content to be below app bar
+	...theme.mixins.toolbar,
+	justifyContent: 'flex-end',
+}));
+
+const menuList = [
+	{ id: '0', name: 'sentence learning', icon: <MenuBookIcon /> },
+	{ id: '1', name: 'create sentence', icon: <CreateIcon /> },
+	{ id: '2', name: 'menu matain', icon: <LibraryBooksIcon /> },
+];
+
+const drawer = (
+	<List>
+		{menuList.map(v => (
+			<ListItem button key={v.id}>
+				<ListItemIcon>
+					{v.icon}
+				</ListItemIcon>
+				<ListItemText primary={v.name} />
+			</ListItem>
+		))}
+	</List>
+);
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const theme = useTheme();
+	const [open, setOpen] = React.useState(false);
+
+	const handleDrawerToggle = () => {
+		setOpen(!open);
+	};
+
+	return (
+		<div>
+			<Box sx={{ flexGrow: 1 }}>
+				<AppBar position="static">
+					<Toolbar>
+						<IconButton
+							size="large"
+							edge="start"
+							color="inherit"
+							aria-label="menu"
+							sx={{ mr: 2 }}
+							onClick={handleDrawerToggle}
+						>
+							<MenuIcon />
+						</IconButton>
+						<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+							英文句子
+						</Typography>
+						<Button color="inherit">登入</Button>
+					</Toolbar>
+				</AppBar>
+			</Box>
+
+			<Drawer
+				sx={{
+					width: drawerWidth,
+					flexShrink: 0,
+					'& .MuiDrawer-paper': {
+						width: drawerWidth,
+						boxSizing: 'border-box',
+					},
+				}}
+				variant="persistent"
+				anchor="left"
+				open={open}
+			>
+				<DrawerHeader>
+					<IconButton onClick={handleDrawerToggle}>
+						{theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+					</IconButton>
+				</DrawerHeader>
+				<Divider />
+				{drawer}
+			</Drawer>
+		</div>
+	);
 }
 
 export default App;
