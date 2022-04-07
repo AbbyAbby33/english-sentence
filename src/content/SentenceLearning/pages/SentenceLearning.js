f096d7a39439458597426b133d7d6d511c940d2b
Abby <sk573129@gmail.com>
2022-04-08 00:01:46 +0800

    feat: (句子學習) 主題列表樣板

--- src/content/SentenceLearning/pages/SentenceLearning.js
+++ src/content/SentenceLearning/pages/SentenceLearning.js
@@ -1,13 +1,47 @@
-import React from 'react'
+import styled from '@emotion/styled';
 
 export default function SentenceLearning() {
+
+    const TOPIC_LIST = [
+        {id: 't0000', name: '打招呼'},
+        {id: 't0001', name: '邀請'},
+        {id: 't0002', name: '道別'},
+        {id: 't0003', name: '接聽電話'},
+        {id: 't0004', name: '預約'},
+        {id: 't0005', name: '商業用語'},
+        {id: 't0006', name: '財務'},
+        {id: 't0007', name: '工作'},
+        {id: 't0008', name: '道歉'},
+        {id: 't0009', name: '談判'},
+        {id: 't0010', name: '開會'},
+        {id: 't0011', name: '議題搞論'},
+        {id: 't0012', name: '表達意見'},
+    ];
+
+    const Topic = styled.div`
+                        @media (max-width: 600px) {
+                            width: calc(50% - 6px);
+                        }
+                        @media (min-width: 601px) and (max-width: 900px) {
+                            width: calc(25% - 6px);
+                        }
+                        display: inline-block;
+                        color: turquoise;
+                        width: calc(12.5% - 6px);
+                        background: #333;
+                        text-align: center;
+                        padding: 10px;
+                        box-sizing: border-box;
+                        margin: 0 3px 6px;
+                        border-radius: 3px;
+                    `
+
     return (
         <div>
-            <div>SentenceLearning</div>
-            <div>SentenceLearning</div>
-            <div>SentenceLearning</div>
-            <div>SentenceLearning</div>
+            {TOPIC_LIST.map(v => {
+                return <Topic key={v.id}>{v.name}</Topic>
+            })}
         </div>
-        
+
     )
 }
