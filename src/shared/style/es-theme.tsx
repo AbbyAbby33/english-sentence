a2bd04e0723b53a33a2cdd0ba0c81198104e6aab
Abby <sk573129@gmail.com>
2022-04-30 19:28:21 +0800

    refactor: 共用的themeBase

--- src/shared/style/es-theme.tsx
+++ src/shared/style/es-theme.tsx
@@ -1,28 +1,7 @@
 import { createTheme } from '@mui/material/styles';
 
-const theme1 = createTheme({
-    components: {
-        MuiCssBaseline: {
-            styleOverrides: {
-                body: {
-                    // TODO: 以後可能會跟著主題換色
-                    backgroundColor: '#aac5b4',
-                    color: 'rgb(0, 0, 0)',
-                    '& .english-font-family': {
-                        fontFamily: `'Roboto Mono', 'serif', 'Segoe UI', 'Roboto'`
-                    },
-                    '& .m0': {
-                        margin: '0'
-                    }
-                }
-            }
-        }
-    },
-
-    typography: {
-        // Google fonts：'Zen Old Mincho'
-        fontFamily: `'Zen Old Mincho', 'serif', 'Segoe UI', 'Roboto'`,
-    },
+/** 基礎共用theme */
+const themeBase = createTheme({
     palette: {
         primary: {
             main: '#16504b',
@@ -45,25 +24,34 @@ const theme1 = createTheme({
     },
 });
 
-const theme2 = createTheme({
+const theme1 = createTheme(themeBase, {
+    components: {
+        MuiCssBaseline: {
+            styleOverrides: {
+                body: {
+                    // TODO: 以後可能會跟著主題換色
+                    backgroundColor: '#aac5b4',
+                    color: 'rgb(0, 0, 0)',
+                    '& .english-font-family': {
+                        fontFamily: `'Roboto Mono', 'serif', 'Segoe UI', 'Roboto'`
+                    },
+                    '& .m0': {
+                        margin: '0'
+                    }
+                }
+            }
+        }
+    },
+    typography: {
+        // Google fonts：'Zen Old Mincho'
+        fontFamily: `'Zen Old Mincho', 'serif', 'Segoe UI', 'Roboto'`,
+    },
+});
+
+const theme2 = createTheme(themeBase, {
 	palette: {
 		primary: {
 			main: '#5bd9ce',
-		},
-		secondary: {
-			main: '#f50057',
-		},
-		info: {
-			main: '#676F54',
-		},
-		warning: {
-			main: '#FDE74C',
-		},
-		error: {
-			main: '#A93636',
-		},
-		success: {
-			main: '#62dc66',
 		},
 	},
 });
